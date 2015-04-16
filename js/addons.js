4d16e0f1c65886a18c9002f150a93a6175f75bd2
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-04-16 09:50:54 -0700

    new thumbnail gallery styles

--- js/addons.js
+++ js/addons.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
 	})
 
 	$( "#photo-one" ).click(function() {
-		$('#product-image').html('<img class="product-image" src="product-images/1.jpg">');
+		$('#product-image').html('<img class="product-image" src="product-images/1.png">');
 		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-two" ).click(function() {
-		$('#product-image').html('<img class="product-image" src="product-images/2.jpg">');
+		$('#product-image').html('<img class="product-image" src="product-images/2.png');
 		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-three" ).click(function() {
-		$('#product-image').html('<img class="product-image" src="product-images/3.jpg">');
+		$('#product-image').html('<img class="product-image" src="product-images/3.png">');
 		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-four" ).click(function() {
-		$('#product-image').html('<img class="product-image" src="product-images/4.jpg">');
+		$('#product-image').html('<img class="product-image" src="product-images/4.png">');
 		$('#product-image').addClass('active-thumbnails');
 	});
 
