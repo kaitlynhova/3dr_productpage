a8b65eb80c1ca949dd46ccbdbaeac4aafad9466c
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-04-15 18:28:06 -0700

    js fix for thumbnail click gallery

--- js/addons.js
+++ js/addons.js
@@ -5,15 +5,19 @@ $(document).ready(function() {
 
 	$( "#photo-one" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/1.jpg">');
+		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-two" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/2.jpg">');
+		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-three" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/3.jpg">');
+		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-four" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/4.jpg">');
+		$('#product-image').addClass('active-thumbnails');
 	});
 
 });
