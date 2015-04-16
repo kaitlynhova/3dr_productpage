6dfb5000634a5bb7d6de540832cb1dba9a368367
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-04-16 09:58:41 -0700

    js and html clean up

--- js/addons.js
+++ js/addons.js
@@ -5,19 +5,12 @@ $(document).ready(function() {
 
 	$( "#photo-one" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/1.png">');
-		$('#product-image').addClass('active-thumbnails');
-	});
-	$( "#photo-two" ).click(function() {
-		$('#product-image').html('<img class="product-image" src="product-images/2.png">');
-		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-three" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/3.png">');
-		$('#product-image').addClass('active-thumbnails');
 	});
 	$( "#photo-four" ).click(function() {
 		$('#product-image').html('<img class="product-image" src="product-images/4.png">');
-		$('#product-image').addClass('active-thumbnails');
 	});
 
 });
