e951bb5d9b7040cc57524586115a04eed82a6a1f
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-04-15 18:22:17 -0700

    initial thumbnail functions

--- js/addons.js
+++ js/addons.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 	$('#myCarousel').carousel({
-	interval: 20000
+		interval: 20000
 	})
+
+	$( "#photo-one" ).click(function() {
+		$('#product-image').html('<img class="product-image" src="product-images/1.jpg">');
+	});
+	$( "#photo-two" ).click(function() {
+		$('#product-image').html('<img class="product-image" src="product-images/2.jpg">');
+	});
+	$( "#photo-three" ).click(function() {
+		$('#product-image').html('<img class="product-image" src="product-images/3.jpg">');
+	});
+	$( "#photo-four" ).click(function() {
+		$('#product-image').html('<img class="product-image" src="product-images/4.jpg">');
+	});
+
 });
